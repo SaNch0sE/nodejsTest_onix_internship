65200ad8689b9e78bb001a89acc1c9516719c935
SaNch0sE <22202508+SaNch0sE@users.noreply.github.com>
2020-01-21 18:45:39 +0200

    Add files via upload

--- app/script.js
+++ app/script.js
@@ -1,28 +1,30 @@
-function getWeather(data, i){
+function getWeather(data, i) {
 	return data[i].n.weather[0].description;
 }
-function getTemp(data, i){
+function getTemp(data, i) {
 	return data[i].n.main.temp;
 }
-function getDate(data, i){
+function getDate(data, i) {
 	return data[i].n.dt_txt.substring(0, 10);
 }
-function getIcon(data, i, parent){
+function getIcon(data, i, parent) {
 	let img = document.createElement('img'); 
     img.src = 'http://openweathermap.org/img/wn/' + data[i].n.weather[0].icon + '@2x.png';
     parent.appendChild(img);
 }
-function send(){
+function send() {
 	let city = document.getElementById("city").value;
 	if (city) {
 		console.log(city);
 		ajax("sendCity", city, (data) => {
-			table = document.getElementById("t");
+			let parent = document.getElementById('view');
 			for (let i = 0; i < 6; i++) {
-				let node = document.getElementById(i);
-				node.innerHTML = getDate(data, i) + ":<br>" + "T: " + getTemp(data, i) + "&#8451;";
-				document.getElementById(i+6).innerHTML = '';
-				getIcon(data, i, document.getElementById(i+6));
+				let node = document.createElement('DIV');
+				let child = document.createElement('p');
+				child.innerHTML = "<span>" + getDate(data, i) + "</span><br>" + "T: " + getTemp(data, i) + "&#8451;";
+				node.appendChild(child);
+				getIcon(data, i, node);
+				parent.appendChild(node);
 			}
 		});
 	}
